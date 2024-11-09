a1ade22071f3e1b98ae01863d610ff2ab93cab24
Adel Rodríguez <hello@adelrodriguez.com>
2024-11-09 11:15:03 -0400

    build: initial config

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@this/ui/components/button"
+import { Input } from "@this/ui/components/input"
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>This is a heading</h1>
+      <Button>Hello</Button>
+      <Input placeholder="Hello" />
+    </div>
+  )
+}
---
+++ packages/ui/src/blocks/index.ts
@@ -0,0 +1,2 @@
+// Blocks are re-usable component created with multiple UI components, that are
+// re-used throughout your apps.
