885978d091fa7d49105296934232d9c4942f8937
Adel Rodríguez <hello@adelrodriguez.com>
2024-11-29 20:58:04 -0400

    feat(mobile): organize folders

--- apps/mobile/src/app/(tabs)/index.tsx
+++ apps/mobile/src/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { Image, Platform, StyleSheet } from "react-native"
 
-import { HelloWave } from "@/components/HelloWave"
-import ParallaxScrollView from "@/components/ParallaxScrollView"
-import { ThemedText } from "@/components/ThemedText"
-import { ThemedView } from "@/components/ThemedView"
 import { APP_ID, APP_NAME } from "@this/common/constants"
+import { HelloWave } from "~/components/HelloWave"
+import ParallaxScrollView from "~/components/ParallaxScrollView"
+import { ThemedText } from "~/components/ThemedText"
+import { ThemedView } from "~/components/ThemedView"
 
 export default function HomeScreen() {
   return (
@@ -12,7 +12,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require("@/assets/images/partial-react-logo.png")}
+          source={require("~/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
       }
@@ -24,16 +24,16 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it {APP_ID}</ThemedText>
         <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
+          Edit 
+          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> 
+          to see changes. Press 
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: "cmd + d",
               android: "cmd + m",
               web: "F12",
             })}
-          </ThemedText>{" "}
+          </ThemedText> 
           to open developer tools.
         </ThemedText>
       </ThemedView>
@@ -47,11 +47,11 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
+          When you're ready, run 
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> 
+          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText> 
+          directory. This will move the current 
+          <ThemedText type="defaultSemiBold">app</ThemedText> to 
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
