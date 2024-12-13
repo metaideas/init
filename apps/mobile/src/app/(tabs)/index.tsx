3084db5ebf8248d8e09b351a81eafe991959e1b2
Adel Rodríguez <hello@adelrodriguez.com>
2024-12-13 19:40:43 -0400

    chore: update deps

--- apps/mobile/src/app/(tabs)/index.tsx
+++ apps/mobile/src/app/(tabs)/index.tsx
@@ -24,16 +24,16 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it {APP_ID}</ThemedText>
         <ThemedText>
-          Edit 
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> 
-          to see changes. Press 
+          Edit
+          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>
+          to see changes. Press
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: "cmd + d",
               android: "cmd + m",
               web: "F12",
             })}
-          </ThemedText> 
+          </ThemedText>
           to open developer tools.
         </ThemedText>
       </ThemedView>
@@ -47,11 +47,11 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
-          When you're ready, run 
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> 
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText> 
-          directory. This will move the current 
-          <ThemedText type="defaultSemiBold">app</ThemedText> to 
+          When you're ready, run
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>
+          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>
+          directory. This will move the current
+          <ThemedText type="defaultSemiBold">app</ThemedText> to
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
