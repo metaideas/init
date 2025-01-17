5b1d8dd50b4eaf3c4f1fc9ea2d0a8e705fc89987
Adel Rodríguez <hello@adelrodriguez.com>
2025-01-16 22:36:47 -0400

    feat: add support for multiple entrypoints

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -1,10 +1,26 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import { defineConfig } from "wxt"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: "chrome",
+  dev: {
+    server: { port: 3006 },
+  },
   imports: false,
   modules: ["@wxt-dev/module-react"],
   publicDir: "static",
   srcDir: "src",
+  vite: () => ({
+    plugins: [
+      TanStackRouterVite({
+        routesDirectory: "./src/entrypoints/popup/routes",
+        generatedRouteTree: "./src/entrypoints/popup/routeTree.gen.ts",
+      }),
+      TanStackRouterVite({
+        routesDirectory: "./src/entrypoints/options/routes",
+        generatedRouteTree: "./src/entrypoints/options/routeTree.gen.ts",
+      }),
+    ],
+  }),
 })
