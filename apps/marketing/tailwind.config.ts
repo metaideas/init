64e79f70253ea29a97f4507b33fc733e1b474e50
Adel Rodríguez <hello@adelrodriguez.com>
2025-01-18 18:28:08 -0400

    feat: setup marketing app

--- apps/marketing/tailwind.config.ts
+++ apps/marketing/tailwind.config.ts
@@ -1,10 +1,8 @@
-import baseConfig from "@tooling/tailwind/web"
+import baseConfig from "@tooling/tailwind/marketing"
 import type { Config } from "tailwindcss"
 import { fontFamily } from "tailwindcss/defaultTheme"
 
 export default {
-  // We need to append the path to the UI package to the content array so that
-  // those classes are included correctly.
   content: [...baseConfig.content, "../../packages/ui/src/**/*.{ts,tsx}"],
   presets: [baseConfig],
   theme: {
@@ -14,4 +12,5 @@ export default {
       },
     },
   },
+  plugins: [],
 } satisfies Config
