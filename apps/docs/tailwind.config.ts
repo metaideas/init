c2e821611180be19f9a2df5afea9b3c45758fd6b
Adel Rodríguez <hello@adelrodriguez.com>
2025-01-19 18:26:17 -0400

    feat: add docs app

---
+++ apps/docs/tailwind.config.ts
@@ -0,0 +1,17 @@
+import starlight from "@astrojs/starlight-tailwind"
+import astro from "@tooling/tailwind/astro"
+import type { Config } from "tailwindcss"
+import { fontFamily } from "tailwindcss/defaultTheme"
+
+export default {
+  content: [...astro.content, "../../packages/ui/src/**/*.{ts,tsx}"],
+  presets: [astro],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [starlight()],
+} satisfies Config
