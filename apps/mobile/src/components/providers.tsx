1269be3b8ca11e857c919b5d93f1e160077f04b6
Adel Rodríguez <hello@adelrodriguez.com>
2025-01-30 19:11:50 -0400

    feat: improve TRPC client setup

--- apps/mobile/src/components/providers.tsx
+++ apps/mobile/src/components/providers.tsx
@@ -1,29 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { transformer } from "@this/common/utils/trpc"
-import { isDevelopment } from "@this/common/variables"
-import { httpBatchLink, httpLink, loggerLink, splitLink } from "@trpc/client"
 import type { ReactNode } from "react"
-import { trpc } from "~/lib/trpc"
-import { buildApiUrl } from "~/lib/utils"
+import { trpc, trpcReact } from "~/lib/trpc"
 
-const url = buildApiUrl("/trpc")
 const queryClient = new QueryClient()
-
-const trpcClient = trpc.createClient({
-  links: [
-    splitLink({
-      // Skip batching for all operations with context property `skipBatch`
-      condition: op => Boolean(op.context.skipBatch),
-      true: httpLink({ url, transformer }),
-      false: httpBatchLink({ url, transformer }),
-    }),
-    loggerLink({ enabled: () => isDevelopment }),
-  ],
-})
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+    <trpc.Provider client={trpcReact} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </trpc.Provider>
   )
--- apps/mobile/src/lib/trpc.ts
+++ apps/mobile/src/lib/trpc.ts
@@ -1,4 +1,30 @@
+import { transformer } from "@this/common/utils/trpc"
+import { isDevelopment } from "@this/common/variables"
+import {
+  type TRPCLink,
+  createTRPCClient,
+  httpBatchLink,
+  httpLink,
+  loggerLink,
+  splitLink,
+} from "@trpc/client"
 import { createTRPCReact } from "@trpc/react-query"
 import type { TRPCClient } from "api/client"
+import { buildApiUrl } from "~/lib/utils"
+
+const url = buildApiUrl("/trpc")
 
 export const trpc = createTRPCReact<TRPCClient>()
+
+const links: TRPCLink<TRPCClient>[] = [
+  loggerLink({ enabled: () => isDevelopment }),
+  splitLink({
+    condition: op => Boolean(op.context.skipBatch),
+    true: httpLink({ url, transformer }),
+    false: httpBatchLink({ url, transformer }),
+  }),
+]
+
+export const trpcReact = trpc.createClient({ links })
+
+export const trpcClient = createTRPCClient<TRPCClient>({ links })
