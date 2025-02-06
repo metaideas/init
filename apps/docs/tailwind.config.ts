f2e5df52092950eae527efc15ace964c3c7ab8e1
Adel Rodríguez <hello@adelrodriguez.com>
2025-02-05 21:58:17 -0400

    feat: remove tailwind config from docs

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,10 +1,11 @@
-import base from "@tooling/tailwind/config"
 import { createPreset } from "fumadocs-ui/tailwind-plugin"
 import type { Config } from "tailwindcss"
 
 export default {
   content: [
-    ...base.content,
+    "./src/assets/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     "./mdx-components.{ts,tsx}",
     "./node_modules/fumadocs-ui/dist/**/*.js",
   ],
