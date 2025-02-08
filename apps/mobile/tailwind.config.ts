faec71d84a5d2fe67de2de1bd06d5f34426bed53
Adel Rodríguez <hello@adelrodriguez.com>
2025-02-08 17:23:34 -0400

    feat: create separate tailwind config for native-ui

--- apps/mobile/tailwind.config.ts
+++ apps/mobile/tailwind.config.ts
@@ -2,26 +2,18 @@
 import nativewind from "nativewind/preset"
 import { hairlineWidth } from "nativewind/theme"
 import type { Config } from "tailwindcss"
-import defaultTheme from "tailwindcss/defaultTheme"
 
-import base from "@tooling/tailwind/config"
+import ui from "@this/native-ui/tailwind.config"
 
 export default {
-  content: [
-    ...base.content,
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  presets: [base, nativewind],
+  content: ui.content,
+  presets: [ui, nativewind],
   theme: {
     extend: {
       borderWidth: {
         // Again, Nativewind is not typed.
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
         hairline: hairlineWidth(),
-      },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
       },
     },
   },
--- packages/utils/src/helpers/classname.ts
+++ packages/utils/src/helpers/classname.ts
@@ -1,6 +1,16 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { twMerge as _twMerge } from "tailwind-merge2"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+/**
+ * This is a temporary helper using the old version of tailwind-merge. Version 3
+ * drops support for earlier Tailwind versions, and we have to wait for
+ * Nativewind to support Tailwind 4.
+ */
+export function _cn(...inputs: ClassValue[]) {
+  return _twMerge(clsx(inputs))
 }
