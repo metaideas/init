d59e940741193402d7e0051e94659633a59113ba
Adel Rodríguez <hello@adelrodriguez.com>
2025-02-08 14:00:44 -0400

    build: delete common and validation packages

--- packages/common/src/utils/assert.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Asserts that a value is never, and throws an error if it is. Use this to make
- * sure that all cases in a `switch` statement are handled.
- */
-export function assertUnreachable(x: never): never {
-  throw new Error(`Case not handled: ${x}`)
-}
