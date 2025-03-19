07cfd5a766beaf9c9c58cfdba1386b21c27ee35d
Adel Rodríguez <hello@adelrodriguez.com>
2025-03-19 12:08:30 -0400

    feat: make popup better

--- apps/extension/src/routes/popup.tsx
+++ apps/extension/src/routes/popup.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router"
 
 import { Button } from "@init/ui/button"
+import { TypographyH1 } from "@init/ui/typography"
 
 import { getTestService } from "~/shared/services"
 
@@ -12,15 +13,17 @@ export const Route = createFileRoute("/popup")({
 
 function Component() {
   return (
-    <div className="h-[500px] w-[500px] bg-red-500">
-      <h1>Popup works!</h1>
-      <Button
-        onClick={() => {
-          testService.test()
-        }}
-      >
-        Testing this
-      </Button>
+    <div className=" h-[500px] w-[500px]">
+      <div className="flex h-full flex-col items-center justify-center gap-4">
+        <TypographyH1>Popup works!</TypographyH1>
+        <Button
+          onClick={() => {
+            testService.test()
+          }}
+        >
+          Testing this
+        </Button>
+      </div>
     </div>
   )
 }
