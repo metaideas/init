3d137fdd2c5a226e5096114962227ee20682d9f3
Adel Rodríguez <hello@adelrodriguez.com>
2025-05-09 15:35:35 -0400

    feat: add lru cache

--- packages/utils/src/helpers/cache.ts
+++ packages/utils/src/helpers/cache.ts
@@ -19,3 +19,45 @@ export function buildKeyGenerator<const T extends string>(): <
     return [category, ...args].join(":") as KeyFormat<C, Args>
   }
 }
+
+export class LRU<Key extends PropertyKey, Value = unknown> {
+  #capacity: number
+  #cache: Map<Key, Value>
+
+  constructor(capacity: number) {
+    this.#capacity = capacity
+    this.#cache = new Map()
+  }
+
+  #setMostRecentlyUsed(key: Key, value: Value) {
+    this.#cache.delete(key)
+    this.#cache.set(key, value)
+  }
+
+  get(key: Key): Value | undefined {
+    const value = this.#cache.get(key)
+
+    if (value === undefined) {
+      return undefined
+    }
+
+    this.#setMostRecentlyUsed(key, value)
+
+    return value
+  }
+
+  set(key: Key, value: Value) {
+    this.#setMostRecentlyUsed(key, value)
+
+    if (this.#cache.size > this.#capacity) {
+      const oldestKey = this.#cache.keys().next().value
+      if (oldestKey !== undefined) {
+        this.#cache.delete(oldestKey)
+      }
+    }
+  }
+
+  delete(key: Key) {
+    this.#cache.delete(key)
+  }
+}
