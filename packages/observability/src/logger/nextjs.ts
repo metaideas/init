4ae05074cb3d5a8c63c2d5e0e4dc391ffa0dc91a
Adel Rodríguez <hello@adelrodriguez.com>
2025-05-13 18:46:23 -0400

    feat: export more log options for nextjs

--- packages/observability/src/logger/nextjs.ts
+++ packages/observability/src/logger/nextjs.ts
@@ -1,1 +1,7 @@
-export { withAxiom as withLogging, AxiomWebVitals } from "next-axiom"
+export {
+  withAxiom as withLogging,
+  AxiomWebVitals,
+  useLogger,
+  Logger,
+  LogLevel,
+} from "next-axiom"
