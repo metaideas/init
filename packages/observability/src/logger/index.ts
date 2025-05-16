5122e96db6a4d79ae66e24bf9811f0bcc1fed69a
Adel Rodríguez <hello@adelrodriguez.com>
2025-05-16 17:17:28 -0400

    fix: add remember to logger

--- packages/observability/src/logger/index.ts
+++ packages/observability/src/logger/index.ts
@@ -1,10 +1,12 @@
 import pino from "pino"
 import pretty from "pino-pretty"
 
+import { remember } from "@init/utils/remember"
+
 /**
  * This is the default logger to be used in other packages. For applications,
  * use the respective logger for the environment.
  */
-export const logger = pino(pretty())
+export const logger = remember("logger", () => pino(pretty({ colorize: true })))
 
 export { default as styles } from "chalk"
