26520aa49849bd97c0ba2682f9d0e90c3187651d
Adel Rodríguez <hello@adelrodriguez.com>
2025-05-21 17:26:11 -0400

    feat: more logger options

--- packages/observability/src/logger/index.ts
+++ packages/observability/src/logger/index.ts
@@ -1,12 +1,27 @@
 import pino from "pino"
 import pretty from "pino-pretty"
 
-import { remember } from "@init/utils/remember"
+import { isDevelopment } from "@init/utils/environment"
 
 /**
  * This is the default logger to be used in other packages. For applications,
  * use the respective logger for the environment.
  */
-export const logger = remember("logger", () => pino(pretty({ colorize: true })))
+export const logger = pino(
+  {
+    level: isDevelopment ? "debug" : "info",
+    redact: {
+      paths: [
+        "password",
+        "secret",
+        "*.secret",
+        "*.password",
+        "req.headers.authorization",
+      ],
+      censor: "[REDACTED]",
+    },
+  },
+  pretty({ colorize: true })
+)
 
 export { default as styles } from "chalk"
