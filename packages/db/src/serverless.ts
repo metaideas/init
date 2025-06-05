a0fee6d4b52723ef7175fd0114d07df1c156f416
Adel Rodríguez <hello@adelrodriguez.com>
2025-06-05 15:40:47 -0400

    chore: update drizzle version

--- packages/db/src/serverless.ts
+++ packages/db/src/serverless.ts
@@ -5,13 +5,18 @@ import envCore from "@init/env/core"
 import env from "@init/env/db"
 import { remember } from "@init/utils/remember"
 
+import type { DrizzleConfig } from "drizzle-orm"
 import * as schema from "./schema"
+
+const config: DrizzleConfig = {
+  casing: "snake_case",
+}
 
 export const db = remember("db-serverless", () => {
   // In development, we can't use the HTTP driver so we'll connect using TCP
   if (envCore.NODE_ENV === "development") {
-    return drizzlePg(env.DATABASE_URL, { schema, casing: "snake_case" })
+    return drizzlePg(env.DATABASE_URL, { ...config, schema })
   }
 
-  return drizzleNeon(env.DATABASE_URL, { schema, casing: "snake_case" })
+  return drizzleNeon(env.DATABASE_URL, { ...config, schema })
 })
