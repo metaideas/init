4c9014f98db496b16cba47dccd8d034b5a214160
Adel Rodríguez <hello@adelrodriguez.com>
2026-01-29 19:30:27 -0400

    fix: update url env var validation

--- apps/app/src/shared/env.ts
+++ apps/app/src/shared/env.ts
@@ -1,14 +1,13 @@
 import { createEnv } from "@init/env"
 import { auth, db, node } from "@init/env/presets"
 import { REACT_PUBLIC_ENV_PREFIX } from "@init/utils/constants"
-import { isCI } from "@init/utils/environment"
+import { isCI, isProduction } from "@init/utils/environment"
 import * as z from "@init/utils/schema"
-import { addProtocol } from "@init/utils/url"
 
 export default createEnv({
   client: {
     PUBLIC_API_URL: z.url().optional(),
-    PUBLIC_BASE_URL: z.string().pipe(z.preprocess((url) => addProtocol(url), z.url())),
+    PUBLIC_BASE_URL: z.url()
   },
   clientPrefix: REACT_PUBLIC_ENV_PREFIX,
   extends: [node(), auth(), auth.providers.github(), auth.providers.google(), db()],
