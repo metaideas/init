8b98891f2103a7ce556998445933d374f00ce00a
Adel Rodríguez <hello@adelrodriguez.com>
2026-02-06 11:56:13 -0400

    fix: remove unnecessary eslint disable comments

--- apps/docs/src/content.config.ts
+++ apps/docs/src/content.config.ts
@@ -3,6 +3,5 @@ import { docsSchema } from "@astrojs/starlight/schema"
 import { defineCollection } from "astro:content"
 
 export const collections = {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 }
--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,13 +1,10 @@
 import { defineMiddleware } from "astro:middleware"
 import { type Locale, overwriteGetLocale } from "#shared/internationalization/runtime.js"
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export const onRequest = defineMiddleware((context, next) => {
   // Grab the locale from Astro based on the URL and overwrite the `getLocale`
   // function to use it
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
   overwriteGetLocale(() => context.currentLocale as Locale)
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
   return next()
 })
