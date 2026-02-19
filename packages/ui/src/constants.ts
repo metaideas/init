acb844a13f66100935d4cb56ca0ea4ac4265fd45
Adel Rodríguez <hello@adelrodriguez.com>
2026-02-18 22:34:56 -0400

    refactor: move MOBILE_BREAKPOINT from utils to ui package

--- packages/ui/src/constants.ts
+++ packages/ui/src/constants.ts
@@ -2,3 +2,4 @@ export const THEMES = ["light", "dark", "system"] as const
 export type Theme = (typeof THEMES)[number]
 
 export const THEME_STORAGE_KEY = "init-theme"
+export const MOBILE_BREAKPOINT = 768
